b33ffbbbff13b847949952c7dad4fa3215ba4e64
kleberarmijos <kleberarmijosf@gmail.com>
2016-05-20 18:01:55 -0500

    coche

---
+++ consecionaria/src/consecionaria/COCHE.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consecionaria;
+
+/**
+ *
+ * @author Usuario
+ */
+public class COCHE {
+    String color;
+
+    public COCHE(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+}
